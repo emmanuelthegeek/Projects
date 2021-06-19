8d154375a3bdab74696a6c8d24e14d6760e26966
HollasII <nicholasolajide@gmail.com>
2021-06-19 20:48:50 +0100

    Update blocker.py

--- blocker.py
+++ blocker.py
@@ -2,8 +2,8 @@ from datetime import datetime as time1
 import time
 host = "C:/Windows/System32/drivers/etc/hosts"
 redirect = "127.0.0.1"
-sites = ["www.facebook.com"]
-print("Begin")
+sites = [input("Enter your website: ")]
+print("Your website has been blocked for the specified time")
 while True:
     start = time1(time1.now().year, time1.now().month, time1.now().day, 8)
     present_time = time1.now()
